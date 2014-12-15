0a2794fa4c812ecd32ae659752fdc713b2c6b5ee
asketola <star7anne@yahoo.com>
2014-12-14 22:32:15 -0800

    Final
    tutorial on adding images

--- Day 11 - ImagePicker/ViewController.swift
+++ Day 11 - ImagePicker/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+ 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func imagesButtonPressed(sender: AnyObject) {
+        
+        var imagePicker = UIImagePickerController() // <- method we are calling to do stuff with pictures
+        
+        var sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum // <- tells where to get the images from
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {    // <- says what to do if you pick an image
+            imagePicker.sourceType = sourceType   // <- calls that source
+            imagePicker.delegate = self           // <- calls the ????
+            
+            //shows a screeen
+            
+            presentViewController(imagePicker, animated: true, completion: nil) // <- shows the screen, and pops it up with animation, doesn't require you to do anything else (the nil part)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        
+        
+        var image = info[UIImagePickerControllerOriginalImage] as UIImage  // <- tells what format to present the image as
+        imageView.image = image // <- loads the image
+        
+        dismissViewControllerAnimated(true, completion: nil) // <- makes the photolibrary go away when you pick the image, with animation
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        println("cancel")
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
+
 
